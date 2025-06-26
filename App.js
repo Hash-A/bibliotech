4bf7f8e6e7ac60ac7075758e981531e15bfab78d
Hashem <yjy8ez@virginia.edu>
2025-06-27 02:36:17 +0300

    Initial commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text> i just pooped my pants! :3</Text>
       <StatusBar style="auto" />
     </View>
   );
